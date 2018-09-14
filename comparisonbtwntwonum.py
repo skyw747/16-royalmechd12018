9d9667ddd4f34c660d0e68dc94a27e6c3db4734e
skyw747 <42933049+skyw747@users.noreply.github.com>
2018-09-14 12:45:24 +0530

    Add files via upload

---
+++ comparisonbtwntwonum.py
@@ -0,0 +1,10 @@
+a=int(input("first number:"))
+b=int(input("second number:"))
+if(a>b):
+	print("a is greater than b")
+if(b>a):
+	print("b is greater than a")
+if(a==b):
+	print("a is equal to b!")
+
+
---
+++ conditionoperator.py
@@ -0,0 +1,8 @@
+a=int(input("type anumber to be tested:"))
+if(a%2==0):
+	print("that's an even one!")
+else:
+	print("that's an odd one!")
+
+
+
