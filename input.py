4164123b037eb156168f631b5de1dbabcbf67f3d
skyw747 <42933049+skyw747@users.noreply.github.com>
2018-09-14 11:32:59 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,8 @@
+a=int(input("enter value of a:"))
+b=int(input("enter value of b:"))
+print('sum:',a+b)
+print('Diference', a-b)
+print('division',a/b)
+print('product', a*b)
+print("end of program")
+
---
+++ operators.py
@@ -0,0 +1,7 @@
+a=20
+b=20
+print('sum is:',a+b)
+print('diff. is:',a-b)
+print('division is:',a/b)
+print('product is:',a*b)
+print("end of program :)")
