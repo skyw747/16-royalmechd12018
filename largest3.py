aa81b27a05c94b03b4cad882a05a1b80d1f15add
Siddharth Mishra <42933049+skyw747@users.noreply.github.com>
2018-09-17 16:05:09 +0530

    Add files via upload

---
+++ largest3.py
@@ -0,0 +1,10 @@
+a=int(input('enter a number:'))
+b=int(input('enter a number:'))
+c=int(input('enter a number:'))
+if(a>b & a>c):
+	print('a is the largest number!')
+elif(b>c):
+	print('b is largest!')
+else:
+	print('c is largest!')
+
