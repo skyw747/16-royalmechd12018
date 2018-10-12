47a1f37e675bb1c128b785bb29ac073b63cc7d92
Siddharth Mishra <42933049+skyw747@users.noreply.github.com>
2018-10-12 12:32:48 +0530

    Add files via upload

---
+++ lab9.py
@@ -0,0 +1,11 @@
+import smtplib
+
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+#Next, log in to the server
+server.login("siddharthmishra1537@gmail.com", "**********")
+
+#Send the mail
+msg = "type program and text ra rae (-_-)"
+server.sendmail("siddharthmishra1537@gmail.com", "thennavanpmohan@gmail.com", msg)
+server.quit()
