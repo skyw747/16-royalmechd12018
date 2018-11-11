c7927b4a20b3cc52e312d4d6cb2d7fabf77df04c
Siddharth Mishra <42933049+skyw747@users.noreply.github.com>
2018-11-11 12:48:27 +0530

    Update and rename lol.py to lab152.py

--- lab152.py
+++ lab152.py
@@ -7,11 +7,11 @@ u = []
 with open('lab.csv','r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
-        #x.append(float(row[0]))
-        #y.append(float(row[1]))
+        x.append(float(row[0]))
+        y.append(float(row[1]))
         z.append(float(row[2]))
         u.append(float(row[3]))
-#plt.plot(x,y, label='Forward Bias', color='orange')
+plt.plot(x,y, label='Forward Bias', color='orange')
 plt.plot(z,u, label='reverse Bias', color='green')
 plt.xlabel('x')
 plt.ylabel('y')
